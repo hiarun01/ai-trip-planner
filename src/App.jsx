2d19a7c245375b38207e9d3456e644323148adaa
Itsarun01 <hey.arunkumar05@gmail.com>
2025-02-28 17:11:46 +0530

    create some file and and setup routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import "./App.css";
 
+import {createBrowserRouter, RouterProvider} from "react-router";
+import AppLayout from "./components/Layout/AppLayout";
+import CreateTrip from "./components/UI/create-trip/CreateTrip";
+import Hero from "./components/UI/Hero";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+
+    children: [
+      {
+        path: "/",
+        element: <Hero />,
+      },
+      {
+        path: "/create-trip",
+        element: <CreateTrip />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <h1 class="text-1xl text-center">Hi</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
